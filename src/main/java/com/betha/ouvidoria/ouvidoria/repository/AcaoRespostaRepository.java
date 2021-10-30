421633055cb098a08e628575c8784f8eddffa42b
Nicolasod <nicolasouriquesdurante@gmail.com>
2021-10-30 09:38:52 -0300

    Att novo repository

---
+++ src/main/java/com/betha/ouvidoria/ouvidoria/repository/AcaoRespostaRepository.java
@@ -0,0 +1,11 @@
+package com.betha.ouvidoria.ouvidoria.repository;
+
+import com.betha.ouvidoria.ouvidoria.model.AcaoResposta;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AcaoRespostaRepository {
+    List<AcaoResposta> findByNome(String tipoResposta);
+}
