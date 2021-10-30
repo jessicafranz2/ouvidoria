6941f0054a2c80013a5bc57c74b21ea00e7969c5
Nicolasod <nicolasouriquesdurante@gmail.com>
2021-10-30 10:04:09 -0300

    att controller

--- src/main/java/com/betha/ouvidoria/ouvidoria/repository/AcaoRespostaRepository.java
+++ src/main/java/com/betha/ouvidoria/ouvidoria/repository/AcaoRespostaRepository.java
@@ -1,11 +1,12 @@
 package com.betha.ouvidoria.ouvidoria.repository;
 
 import com.betha.ouvidoria.ouvidoria.model.AcaoResposta;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface AcaoRespostaRepository {
+public interface AcaoRespostaRepository extends JpaRepository<AcaoResposta, Long> {
     List<AcaoResposta> findByNome(String tipoResposta);
 }
---
+++ src/main/java/com/betha/ouvidoria/ouvidoria/resource/AcaoRespostaDTO.java
@@ -0,0 +1,39 @@
+package com.betha.ouvidoria.ouvidoria.resource;
+
+import com.betha.ouvidoria.ouvidoria.model.AcaoResposta;
+import com.betha.ouvidoria.ouvidoria.model.Pais;
+
+public class AcaoRespostaDTO {
+    private Long id;
+    private String tipoResposta;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipoResposta() {
+        return tipoResposta;
+    }
+
+    public void setTipoResposta(String tipoResposta) {
+        this.tipoResposta = tipoResposta;
+    }
+
+    public static AcaoRespostaDTO toDTO(AcaoResposta acaoResposta){
+        AcaoRespostaDTO dto = new AcaoRespostaDTO();
+        dto.setId(acaoResposta.getId());
+        dto.setTipoResposta(acaoResposta.getTipoResposta());
+        return dto;
+    }
+
+    public static AcaoResposta fromDTO(AcaoRespostaDTO dto){
+        AcaoResposta entity = new Pais();
+        entity.setId(dto.getId());
+        entity.setTipoResposta(dto.getTipoResposta());
+        return  entity;
+    }
+}
